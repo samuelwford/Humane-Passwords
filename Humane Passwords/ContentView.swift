63d38913ee129a96aec0ad8847e4d9a3f1c00ec2
Samuel Ford <samuel.ford@icloud.com>
2021-06-06 12:31:25 -0400

    Initial Commit

---
+++ Humane Passwords/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Humane Passwords
+//
+//  Created by Samuel Ford on 6/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Humane Passwords/Humane_PasswordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Humane_PasswordsApp.swift
+//  Humane Passwords
+//
+//  Created by Samuel Ford on 6/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Humane_PasswordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
